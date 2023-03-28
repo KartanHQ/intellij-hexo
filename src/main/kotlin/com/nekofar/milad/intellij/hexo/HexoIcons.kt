4e8c562c727cad5c3784405daa1e583bbd1d0502
Milad Nekofar <milad@nekofar.com>
2023-03-28 09:28:24 +0330

    refactor: update plugin icon and add extra icons

---
+++ src/main/kotlin/com/nekofar/milad/intellij/hexo/HexoIcons.kt
@@ -0,0 +1,11 @@
+package com.nekofar.milad.intellij.hexo
+
+import com.intellij.openapi.util.IconLoader
+
+object HexoIcons {
+    @JvmField
+    val FileType = IconLoader.getIcon("/icons/type.svg", javaClass)
+
+    @JvmField
+    val ProjectGenerator = IconLoader.getIcon("/icons/hexo.svg", javaClass)
+}
