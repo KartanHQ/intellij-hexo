b4c9ab0eb82e513b3dd9aa1b2718f6243bed91aa
Milad Nekofar <milad@nekofar.com>
2023-07-27 10:10:11 +0330

    refactor: clean up project generator and simplify code
    Refactored HexoCliProjectGenerator.kt, HexoCliProjectModuleBuilder.kt, and HexoProjectTemplateFactory.kt. Improved readability by replacing "HexoBundle.message" with "message". Renamed parameters in "customizeModule" for clarity. Simplified some method calls by removing redundant code (e.g. converting getNpxCommands() to expression body). Moved instantiation of HexoCliProjectGenerator in latter two files to the next lines for improved readability. This commit aims to improve code efficiency and maintainability.

--- src/main/kotlin/com/nekofar/milad/intellij/hexo/cli/HexoCliProjectGenerator.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/hexo/cli/HexoCliProjectGenerator.kt
@@ -6,34 +6,31 @@ import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.vfs.VirtualFile
-import com.nekofar.milad.intellij.hexo.HexoBundle
+import com.nekofar.milad.intellij.hexo.HexoBundle.message
 import com.nekofar.milad.intellij.hexo.HexoIcons
-import javax.swing.Icon
 
 class HexoCliProjectGenerator : NpmPackageProjectGenerator() {
     private data class PackageInfo(val name: String, val executable: String, val command: String)
 
     private val packageInfo = PackageInfo("hexo", "hexo", "init")
 
-    override fun getName() = HexoBundle.message("hexo.project.generator.name")
+    override fun getName() = message("hexo.project.generator.name")
 
-    override fun getDescription() = HexoBundle.message("hexo.project.generator.description")
+    override fun getDescription() = message("hexo.project.generator.description")
 
     override fun filters(project: Project, baseDir: VirtualFile) = emptyArray<Filter>()
 
-    override fun customizeModule(p0: VirtualFile, p1: ContentEntry?) {}
+    override fun customizeModule(virtualFile: VirtualFile, contentEntry: ContentEntry?) {}
 
     override fun packageName() = packageInfo.name
 
-    override fun presentablePackageName() = HexoBundle.message("hexo.project.generator.presentable.package.name")
+    override fun presentablePackageName() = message("hexo.project.generator.presentable.package.name")
 
-    override fun getNpxCommands(): List<NpxPackageDescriptor.NpxCommand> =
-        listOf(NpxPackageDescriptor.NpxCommand(packageInfo.name, packageInfo.executable))
+    override fun getNpxCommands() = listOf(NpxPackageDescriptor.NpxCommand(packageInfo.name, packageInfo.executable))
 
     override fun generateInTemp() = true
 
-    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?) =
-        arrayOf(packageInfo.command, project?.name.toString())
+    override fun generatorArgs(project: Project, baseDir: VirtualFile) = arrayOf(packageInfo.command, project.name)
 
-    override fun getIcon(): Icon = HexoIcons.ProjectGenerator
+    override fun getIcon() = HexoIcons.ProjectGenerator
 }
--- src/main/kotlin/com/nekofar/milad/intellij/hexo/cli/HexoCliProjectModuleBuilder.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/hexo/cli/HexoCliProjectModuleBuilder.kt
@@ -3,4 +3,6 @@ package com.nekofar.milad.intellij.hexo.cli
 import com.intellij.ide.util.projectWizard.WebTemplateNewProjectWizard
 import com.intellij.ide.wizard.GeneratorNewProjectWizardBuilderAdapter
 
-class HexoCliProjectModuleBuilder : GeneratorNewProjectWizardBuilderAdapter(WebTemplateNewProjectWizard(HexoCliProjectGenerator()))
+class HexoCliProjectModuleBuilder : GeneratorNewProjectWizardBuilderAdapter(WebTemplateNewProjectWizard(
+    HexoCliProjectGenerator()
+))
